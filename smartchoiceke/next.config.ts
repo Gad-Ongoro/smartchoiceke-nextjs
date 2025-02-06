19d20bbf837070b4932465791098cb4e0b6e599a
Gad-Ongoro <gadongoro1@gmail.com>
2025-02-06 15:29:01 +0300

    feat: external images config

--- smartchoiceke/next.config.ts
+++ smartchoiceke/next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+
 };
 
 export default nextConfig;
+
+module.exports = {
+  images: {
+    domains: ['i.pinimg.com', 'hebbkx1anhila5yf.public.blob.vercel-storage.com'],
+  },
+};
